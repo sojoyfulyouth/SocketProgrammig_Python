9178d61863a27217bbe56e63a8b77aab60880176
sojoyfulyouth <thdwldbs01@gmail.com>
2023-05-22 20:25:49 +0900

    complete stringtransfer and create fileModifier folder

---
+++ FileModifier/client.py
@@ -0,0 +1,19 @@
+import socket
+
+SERVER = "127.0.0.1"
+PORT = 8080
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((SERVER, PORT))
+while True:
+    inp = input("Enter the string : ")
+    if inp == "Over":
+        break
+    client.send(inp.encode())
+
+    answer = client.recv(1024)
+    print("Answer is "+answer.decode())
+    print("Type 'Over' to terminate")
+
+client.close()
